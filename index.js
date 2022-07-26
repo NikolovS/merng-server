c8455297158a24aedbe08a98d239287d9516e2ec
Stefan Nikolov <stefan.nikolov@abv.bg>
2022-07-26 16:44:37 +0300

    sercond

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const { MONGODB } = require('./config');
 // Adding subscription
 // const pubsub = new PubSub();
 
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 
 const server = new ApolloServer({
   typeDefs,
